19e5c9307a90a00cee97aeedf25584868348d9d4
Aleksandr <baykov-1988@mail.ru>
2024-12-05 23:06:21 +0300

    modified:   src/components/app/app.tsx 	new file:   src/components/private-route/private-route.tsx 	new file:   src/const.tsx

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -5,7 +5,9 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Login } from '../../pages/login/login';
 import { Favorites } from '../../pages/favorites/favorites';
 import { Property } from '../../pages/property/property';
-import { NotFound } from '../../pages/not-found/not-found';
+//import { NotFound } from '../../pages/not-found/not-found';
+import { PrivateRoute } from '../private-route/private-route';
+import { AppRoute, AuthorizationStatus } from '../../const';
 
 type AppProps = {
   offersCount: number
@@ -15,12 +17,20 @@ const App = ({ offersCount }: AppProps): JSX.Element => (
   <BrowserRouter>
     <Routes>
       <Route index element={<Main offersCount={offersCount} />}/>
-      <Route path="/login" element={<Login />} />
-      <Route path="/favorites" element={<Favorites />} />
-      <Route path="/offer/:id" element={<Property />} />
-      <Route path="*" element={<NotFound />} />
+      <Route path={AppRoute.Login} element={<Login />} />
+      <Route path={`${AppRoute.Property}/:id`} element={<Property />} />
+      <Route path={AppRoute.Favorites}
+        element={
+          <PrivateRoute
+            authorizationStatus={AuthorizationStatus.NoAuth}
+          >
+            <Favorites />
+          </PrivateRoute>
+        }
+      />
     </Routes>
   </BrowserRouter>
 );
+
 
 export default App;
