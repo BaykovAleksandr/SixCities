e84d3aa2521cbf6fbd1421e7f798477b8a355f59
Aleksandr <baykov-1988@mail.ru>
2024-12-13 20:12:14 +0300

    modified:   src/components/sorting-list/sorting-list.tsx 	modified:   src/const.ts 	modified:   src/store/action.ts 	modified:   src/store/reducer.ts 	modified:   src/types/types.ts

--- project/src/components/sorting-list/sorting-list.tsx
+++ project/src/components/sorting-list/sorting-list.tsx
@@ -1,39 +1,59 @@
+import type { SortName } from '../../types/types';
+
 import { useState } from 'react';
 
-const SortingList = (): JSX.Element => {
+import { Sorting } from '../../const';
+
+
+type SortingListProps = {
+  onChange: (name: SortName) => void;
+  activeSorting: SortName;
+};
+
+const SortingList = ({
+  onChange,
+  activeSorting,
+}: SortingListProps): JSX.Element => {
   const [isOpened, setIsOpened] = useState<boolean>(false);
 
   const handleToggleButtonClick = () => {
     setIsOpened(!isOpened);
   };
 
+  const handleSortItemClick = (name: SortName) => {
+    setIsOpened(false);
+    onChange(name);
+  };
+
   return (
     <form className="places__sorting" action="#" method="get">
       <span className="places__sorting-caption">Sort by</span>
-      <span className="places__sorting-type" tabIndex={0} onClick={handleToggleButtonClick}>
-                  Popular
+      <span
+        className="places__sorting-type"
+        tabIndex={0}
+        onClick={handleToggleButtonClick}
+      >
+        {Sorting[activeSorting]}
         <svg className="places__sorting-arrow" width="7" height="4">
           <use xlinkHref="#icon-arrow-select"></use>
         </svg>
       </span>
       {isOpened && (
         <ul className="places__options places__options--custom places__options--opened">
-          <li
-            className="places__option places__option--active"
-            tabIndex={0}
-          >
-                    Popular
-          </li>
-          <li className="places__option" tabIndex={0}>
-                    Price: low to high
-          </li>
-          <li className="places__option" tabIndex={0}>
-                    Price: high to low
-          </li>
-          <li className="places__option" tabIndex={0}>
-                    Top rated first
-          </li>
-        </ul>)}
+          {(Object.entries(Sorting) as [SortName, Sorting][]).map(([name, title]) => (
+            <li
+              key={name}
+              className={`places__option${name === activeSorting ? ' places__option--active' : ''}`}
+              onClick={() => handleSortItemClick(name)}
+              tabIndex={0}
+            >
+              {title}
+            </li>
+          ))}
+        </ul>
+      )}
     </form>
   );
 };
+
+export default SortingList;
