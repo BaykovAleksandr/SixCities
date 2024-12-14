e9dc408eb42e39a1d0d2e07916ba75bc37d5e7f6
Aleksandr <baykov-1988@mail.ru>
2024-12-14 22:41:38 +0300

    modified:   src/components/private-route/private-route.tsx 	modified:   src/components/review-list/review-list.tsx 	modified:   src/pages/main/main.tsx

--- project/src/components/private-route/private-route.tsx
+++ project/src/components/private-route/private-route.tsx
@@ -1,38 +1,19 @@
-import type { Comment } from '../../types/types';
+import { Navigate } from 'react-router-dom';
+import { AppRoute, AuthorizationStatus } from '../../const';
+import { useAppSelector } from '../../hooks';
 
-import { formatDate, getStarsWidth } from '../../utils';
+type PrivateRouteProps = {
+  children: JSX.Element;
+}
 
-const Review = ({ comment, date, rating, user }: Comment) => {
-  const { name, avatarUrl } = user;
+const PrivateRoute = ({ children }: PrivateRouteProps): JSX.Element => {
+  const authorizationStatus = useAppSelector((state) => state.authorizationStatus);
 
   return (
-    <li className="reviews__item">
-      <div className="reviews__user user">
-        <div className="reviews__avatar-wrapper user__avatar-wrapper">
-          <img className="reviews__avatar user__avatar" src={avatarUrl} width={54} height={54} alt="Reviews avatar" />
-        </div>
-        <span className="reviews__user-name">
-          {name}
-        </span>
-      </div>
-      <div className="reviews__info">
-        <div className="reviews__rating rating">
-          <div className="reviews__stars rating__stars">
-            <span
-              style={{
-                width: getStarsWidth(rating)
-              }}
-            />
-            <span className="visually-hidden">Rating</span>
-          </div>
-        </div>
-        <p className="reviews__text">
-          {comment}
-        </p>
-        <time className="reviews__time" dateTime={date}>{formatDate(date)}</time>
-      </div>
-    </li>
+    authorizationStatus === AuthorizationStatus.Auth
+      ? children
+      : <Navigate to={AppRoute.Login} />
   );
 };
 
-export default Review;
+export default PrivateRoute;
